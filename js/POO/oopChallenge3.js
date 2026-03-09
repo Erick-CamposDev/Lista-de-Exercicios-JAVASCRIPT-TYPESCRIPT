db44204b1bf9904c86711086547b16cef28fcaa2
Erick Fellipe <efrc22br@gmail.com>
2026-03-09 17:26:03 -0300

    Adicionado dois novos exercicios de POO

---
+++ js/POO/oopChallenge3.js
@@ -0,0 +1,58 @@
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(title) {
+    this.books.push(title);
+  }
+
+  borrow(title) {
+    if (title.isAvailable) {
+      console.log(`Você pegou emprestado o livro de ${title.title}`);
+      title.borrowBook();
+    } else {
+      console.log("Livro já foi emprestado, pegue outro.");
+    }
+  }
+
+  return(title) {
+    if (!title.isAvailable) {
+      console.log("O livro emprestado foi devolvido!");
+      title.returnBook();
+    } else {
+      console.log("Livro já foi devolvido.");
+    }
+  }
+
+  listBooks() {
+    if (this.books.length <= 0) {
+      console.log("Não há livros para poder listar!");
+    } else {
+      this.books.forEach((book) => {
+        const confirmation = book.isAvailable === true ? "Sim" : "Não";
+
+        console.log(
+          `----------\nNome do Livro: ${book.title}\nAutor: ${book.author}\nAno: ${book.year}\nDisponível: ${confirmation}\n`,
+        );
+      });
+    }
+  }
+}
+
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.isAvailable = true;
+  }
+
+  borrowBook() {
+    this.isAvailable = false;
+  }
+
+  returnBook() {
+    this.isAvailable = true;
+  }
+}
---
+++ js/POO/oopChallenge4.js
@@ -0,0 +1,67 @@
+class arrayManager {
+  constructor(length) {
+    this.array = [];
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (length < 0) {
+      console.log("O array não pode ter o tamanho menor que 0!");
+      return;
+    }
+
+    if (length < this.array.length) {
+      console.log("O novo tamanho é menor que o array já existente!");
+      return;
+    }
+    this._length = length;
+  }
+
+  addValue(value) {
+    if (this.array.length >= this.length) {
+      console.log(
+        "Não pode adicionar mais nenhum valor ao array, limite máximo atingido!",
+      );
+      return;
+    }
+    this.array[this.array.length] = value;
+  }
+
+  removeValue(value) {
+    let found = false;
+
+    if (this.array.length === 0) {
+      console.log("O array não contém nenhum valor!");
+      return;
+    }
+
+    for (let i = 0; i < this.array.length; i++) {
+      if (this.array[i] === value) {
+        this.array.splice(i, 1);
+        console.log(`O valor ${value} foi retirado!`);
+        found = true;
+        break;
+      }
+
+      if (!found) {
+        console.log("Valor não encontrado! Insire outro valor correspondente!");
+      }
+    }
+  }
+
+  showArray() {
+    console.log(this.array);
+  }
+}
+
+let array = new arrayManager(3);
+
+array.addValue(4);
+array.addValue(4);
+array.addValue(4);
+
+array.length = 1;
